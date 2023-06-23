c9292377d485be6fc0f99e7943d89f98fbfc9a13
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2023-06-23 01:30:49 -0300

    Agregado de Edit y Delete

--- Yucra-Fabricio-Parcial-2/SistemaDeCadenasAlimenticias.Servicio/SucursalesServicio.cs
+++ Yucra-Fabricio-Parcial-2/SistemaDeCadenasAlimenticias.Servicio/SucursalesServicio.cs
@@ -9,9 +9,12 @@ namespace SistemaDeCadenasAlimenticias.Servicio
 {
     public interface ISucursalServicio {
         void Crear(Sucursal sucursal);
+        void Editar(Sucursal sucursal);
+        void EliminarSucursal(int idSucursal);
         List<Sucursal> GetALLSucursales();
         List<Sucursal> GetSucursalesByIdCadena(int idCadena);
-
+        bool SucursalExistente(int value);
+        Sucursal GetSucursalById(int? id);        
     }
     public class SucursalesServicio : ISucursalServicio
     {
@@ -26,14 +29,40 @@ namespace SistemaDeCadenasAlimenticias.Servicio
             _context.SaveChanges();
         }
 
+        public void Editar(Sucursal sucursal)
+        {
+            _context.Sucursals.Update(sucursal);
+            _context.SaveChangesAsync();
+        }
+
         public List<Sucursal> GetALLSucursales()
         {
             return _context.Sucursals.ToList();
-        }
+        }        
 
         public List<Sucursal> GetSucursalesByIdCadena(int idCadena)
         {
             return _context.Sucursals.Where(s => s.IdCadena == idCadena).ToList();
         }
+
+        public bool SucursalExistente(int id)
+        {
+            return (_context.Sucursals.Any(s=>s.Id == id));
+        }
+
+        public Sucursal GetSucursalById(int? id)
+        {
+            return _context.Sucursals.FirstOrDefault(s => s.Id == id);
+        }     
+
+        public void EliminarSucursal(int idSucursal)
+        {
+            var sucursal = _context.Sucursals.Find(idSucursal);
+            if (sucursal != null)
+            {
+                _context.Sucursals.Remove(sucursal);
+                _context.SaveChanges();
+            }
+        }      
     }
 }
--- Yucra-Fabricio-Parcial-2/SistemaDeCadenasAlimenticias.Web/Controllers/SucursalController.cs
+++ Yucra-Fabricio-Parcial-2/SistemaDeCadenasAlimenticias.Web/Controllers/SucursalController.cs
@@ -2,16 +2,17 @@
 using SistemaDeCadenasAlimenticias.Data;
 using SistemaDeCadenasAlimenticias.Data.Entidades;
 using SistemaDeCadenasAlimenticias.Servicio;
+using System.Data.Entity.Infrastructure;
 
 namespace SistemaDeCadenasAlimenticias.Web.Controllers
 {
     public class SucursalController : Controller
     {
-        private ISucursalServicio _sucursalServicio;
+        private ISucursalServicio _sucursalesServicios;
         private ICadenasServicios _cadenasServicios;
         public SucursalController(ISucursalServicio isucursalServicio, ICadenasServicios cadenasServicios)
         {
-            _sucursalServicio = isucursalServicio;
+            _sucursalesServicios = isucursalServicio;
             _cadenasServicios = cadenasServicios;
         }
 
@@ -24,12 +25,12 @@ namespace SistemaDeCadenasAlimenticias.Web.Controllers
             ViewBag.Cadenas = cadenas;          
 
             // Obtener la lista de sucursales
-            List<Sucursal> sucursales = _sucursalServicio.GetALLSucursales();//llama al get del contexto
+            List<Sucursal> sucursales = _sucursalesServicios.GetALLSucursales();//llama al get del contexto
 
             if (idCadena.HasValue) //vemos si el valor q devuelve la View es null, si no lo es hacemos la parte del filtro (.HasValue solo para valores nulleables)
             {
                 // Filtrar por la cadena seleccionada si se proporciona un idCadena válido
-                sucursales = _sucursalServicio.GetSucursalesByIdCadena(idCadena.Value);
+                sucursales = _sucursalesServicios.GetSucursalesByIdCadena(idCadena.Value);
             }
 
             return View(sucursales);// sucursales sera devuelto o todas o la q tiene un filtro depende si es Null o no 
@@ -46,11 +47,89 @@ namespace SistemaDeCadenasAlimenticias.Web.Controllers
             if (ModelState.IsValid)// valida si lo que devolvio la View cumple con las necesidades del modelo que se quiere usar
             {
 
-                _sucursalServicio.Crear(sucursal);// comitea los cambios y se resuelven todos los cambios en la base de datos 
+                _sucursalesServicios.Crear(sucursal);// comitea los cambios y se resuelven todos los cambios en la base de datos 
                 return RedirectToAction("Index");
             }
             ViewBag.Cadenas = _cadenasServicios.GetAllCadenas();
             return View(sucursal);
         }
+
+        // GET: Aulas/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || !_sucursalesServicios.SucursalExistente(id.Value)) 
+            {
+                return NotFound();
+            }
+
+            var sucursal = _sucursalesServicios.GetSucursalById(id.Value);//donde usa el contexto para pasorlo por la view
+                                                                          //
+            if (sucursal == null)
+            {
+                return NotFound();
+            }
+
+            return View(sucursal);
+        }
+        // POST: Aulas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (!_sucursalesServicios.SucursalExistente(id))
+            {
+                return Problem("Entity set 'Sucursal' is null.");
+            }
+
+            _sucursalesServicios.EliminarSucursal(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: ObraDeArtes/Edit/5
+        public IActionResult Edit(int? id)
+        {            
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sucursal = _sucursalesServicios.GetSucursalById(id.Value);
+            if (sucursal == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Cadenas = _cadenasServicios.GetAllCadenas();
+            return View(sucursal);
+        }
+
+        // POST: ObraDeArtes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Sucursal sucursal)
+        {
+            if (id != sucursal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _sucursalesServicios.Editar(sucursal);
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Cadenas = _cadenasServicios.GetAllCadenas();
+            return View(sucursal);
+        }
+
     }
 }
